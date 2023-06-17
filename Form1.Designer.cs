a87819019f191551907a9814a3dabdd8813385b8
unknown <ajimiyaayoobkhan@gmail.com>
2023-06-17 22:57:00 +0530

    claysis

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace login_new
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel3 = new Panel();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            label6 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            label4 = new Label();
+            label3 = new Label();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            panel4 = new Panel();
+            pictureBox3 = new PictureBox();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(78, 71);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(818, 444);
+            panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button3);
+            panel3.Controls.Add(pictureBox5);
+            panel3.Controls.Add(pictureBox4);
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(label5);
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(panel4);
+            panel3.Dock = DockStyle.Right;
+            panel3.Location = new Point(345, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(473, 444);
+            panel3.TabIndex = 0;
+            panel3.Paint += panel3_Paint;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.lock_two;
+            pictureBox5.Location = new Point(423, 230);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(32, 42);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 12;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.lock_two;
+            pictureBox4.Location = new Point(423, 142);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(32, 39);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 11;
+            pictureBox4.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Garamond", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label6.ForeColor = Color.DarkMagenta;
+            label6.Location = new Point(313, 371);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 18);
+            label6.TabIndex = 10;
+            label6.Text = "Sign up";
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Courier New", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(119, 373);
+            label5.Name = "label5";
+            label5.Size = new Size(199, 16);
+            label5.TabIndex = 9;
+            label5.Text = "Don't have an account ? ";
+            label5.Click += label5_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.HotPink;
+            button1.Font = new Font("Myanmar Text", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button1.Location = new Point(327, 321);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 36);
+            button1.TabIndex = 8;
+            button1.Text = "LOGIN";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Garamond", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label4.ForeColor = Color.DarkMagenta;
+            label4.Location = new Point(327, 281);
+            label4.Name = "label4";
+            label4.Size = new Size(128, 18);
+            label4.TabIndex = 7;
+            label4.Text = "Forgot Password ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Courier New", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(59, 281);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 17);
+            label3.TabIndex = 6;
+            label3.Text = "Remember me ";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.lock_pink;
+            pictureBox2.Location = new Point(222, 53);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(66, 46);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Bookman Old Style", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkMagenta;
+            label1.Location = new Point(55, 208);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 19);
+            label1.TabIndex = 4;
+            label1.Text = "Password : ";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(55, 230);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(400, 42);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(55, 142);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(400, 39);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Bookman Old Style", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.DarkMagenta;
+            label2.Location = new Point(55, 120);
+            label2.Name = "label2";
+            label2.Size = new Size(115, 19);
+            label2.TabIndex = 1;
+            label2.Text = "User name  : ";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(pictureBox3);
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(0, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(473, 47);
+            panel4.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.mutiplication;
+            pictureBox3.Location = new Point(423, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(50, 47);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(346, 444);
+            panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.login_two;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(346, 444);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.HotPink;
+            button3.Font = new Font("Myanmar Text", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button3.Location = new Point(61, 321);
+            button3.Name = "button3";
+            button3.Size = new Size(132, 36);
+            button3.TabIndex = 14;
+            button3.Text = "EXIT";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.HotPink;
+            ClientSize = new Size(984, 561);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Login Form";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Panel panel4;
+        private Label label6;
+        private Label label5;
+        private Button button1;
+        private Label label4;
+        private Label label3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_new
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Main ss = new Main();
+            ss.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
